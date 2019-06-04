04501b4e96aa7eb45f3d1a7bae60c6cc63352fbf
Eduard Saadyan <eduard.sdn@gmail.com>
2019-06-04 19:43:06 +0300

    added a text file processing function
    added a text file processing function

--- Price_optimizator.py
+++ Price_optimizator.py
@@ -1,1 +1,12 @@
-#Created initial python file
+
+def read_data_store(file_path): #reads pricelist.txt
+    pricelist = {}
+    data = []
+    with open(file_path, "r") as f:
+        f.readline()
+        for line in f:
+            parts = line.split(",")
+            row = [parts[0], parts[1],int(parts[2])]
+
+            data.append(row)
+    return data
