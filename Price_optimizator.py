5dea73b27b89216cd9f445bb470a1a510e5848ea
GlebFang <51119133+GlebFang@users.noreply.github.com>
2019-06-11 10:05:36 +0300

    Aded json processing

--- Price_optimizator.py
+++ Price_optimizator.py
@@ -6,3 +6,25 @@ def get_document0(file_name):
             parts = line.split(',')
             document0[parts[1]] = int(parts[2])
     return document0
+
+
+def get_document1(file_name, catalog1, catalog2, catalog3):
+    document1 = {}
+    with open(file_name, 'r', encoding='utf-8') as f:
+        data = json.loads(f.read())
+        for item in data[catalog1][catalog2][catalog3]:
+            document1[item['name']] = item['price']
+    return document1
+
+
+def get_document2(file_name, catalog1, catalog2, catalog3):
+    document2 = {}
+    with open(file_name, 'r', encoding='utf-8') as f:
+        data = json.loads(f.read())
+        for item in data[catalog1][catalog2][catalog3]:
+            document2[item['name']] = item['price']
+    return document2
+
+
+document1 = get_document1('Price_optimizatorG_M.video.json', "M.video", "Games_Soft_&_Entertainment", "Games_for_PS4")
+document2 = get_document2('PSStore_saleG.json', "PlayStationStore", "Games", "PS4")
